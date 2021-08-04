eadcb818436f8cbd6856ebc6a3a54bbab1493422
Nerissa Griffiths <ngriffit@villanova.edu>
2021-08-04 21:26:09 +0000

    testing heroku one more time
    Co-authored-by: Eman Mustefa <eamustefa@gmail.com>
    Co-authored-by: Alex Vargas <alexvargas0323@gmail.com>
    Co-authored-by: Federico Gutierrez <fg42768n@pace.edu>

---
+++ config.py
@@ -0,0 +1,24 @@
+'''
+Sensitive data not to be pushed to github,
+only store locally. Download this off Codio
+so that you never lose it.
+'''
+# Google Geocode API
+GEO_KEY = 'AIzaSyA8jYYYxddQbrHHI-9jPUWIVe3O_69nP0A'
+
+# Lufthansa API
+CLIENT_ID = 'jh8jcna2brkavwvwakm7u2jw'
+CLIENT_SECRET = 'HCD4hzTFEfZW7gxd2Z5S'
+
+# Lumo API
+AUTH_KEY = '03fbb74e237943679887fab1849d618b'
+
+# Windy API
+CAM_KEY = "4oAulVBpHcW2ILJPE6nPprmshj7c74wh"
+
+# Travel API
+headers = {
+    'content-type': "application/json",
+    'x-rapidapi-key': "dddd636890msh920b10a66955182p15fca4jsnabf596755ddd",
+    'x-rapidapi-host': "travel-advisor.p.rapidapi.com"
+}
